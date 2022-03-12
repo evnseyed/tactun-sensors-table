d58c24f0d2573eb123cc5e8c612cf2685df4d4be
Evn Sey <evn.seyed@gmail.com>
2022-03-12 18:25:40 +0300

    feat(header): init header and add home button

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import type { VFC } from 'react';
-import './header.scss';
 
+import './header.scss';
+import { HeaderHomeButton } from './header-home-button/header-home-button'
 export const Header: VFC<{}> = () => {
   return (
     <header className="header">
+      <HeaderHomeButton />
     </header>
   );
 }
