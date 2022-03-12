b710c0682f97aea2a294470be8df0bb5d7747cec
Evn Sey <evn.seyed@gmail.com>
2022-03-12 16:06:25 +0300

    feat(header): create story

---
+++ src/header/header.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Header } from './header';
+
+export default {
+  title: 'Header',
+  component: Header,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const Example = Template.bind({});
+// LoggedOut.args = {};
+
+// export const LoggedIn = Template.bind({});
+// LoggedIn.args = {
+//   user: {
+//     name: 'Jane Doe',
+//   },
+// };
+
+// export const LoggedOut = Template.bind({});
+// LoggedOut.args = {};
