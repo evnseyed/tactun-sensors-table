9f31ca8d66f084ea307ee3af4a6b56450b5888d6
Evn Sey <evn.seyed@gmail.com>
2022-03-12 18:25:54 +0300

    feat(app): add header to app

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './app.scss';
 
+import { Header } from './header/header';
+
 export const App = () => {
   return (
     <div className="app">
-      
+      <Header />
     </div>
   );
 }
