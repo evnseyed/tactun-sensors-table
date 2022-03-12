db7bfed7f963e71fabeadf4e4e0ec50402c766f5
Evn Sey <evn.seyed@gmail.com>
2022-03-12 15:48:06 +0300

    style: plug blueprint styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './blueprint.scss'
 import { App } from './app';
 import reportWebVitals from './report-web-vitals';
 
