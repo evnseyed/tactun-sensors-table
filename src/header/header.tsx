f6789eaf6b66731d39187472ce5a1d00d67b979e
Evn Sey <evn.seyed@gmail.com>
2022-03-12 15:49:52 +0300

    feat(header): create blank

---
+++ src/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import type { VFC } from 'react';
+import './header.scss';
+
+export const Header: VFC<{}> = () => {
+  return (
+    <header className="header">
+    </header>
+  );
+}
