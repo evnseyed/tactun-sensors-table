d0190ccac9398c8ab58a5be34c1e904ac0391201
Evn Sey <evn.seyed@gmail.com>
2022-03-12 19:17:24 +0300

    feat(header): add header buttons into header

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import type { VFC } from 'react';
 
 import './header.scss';
-import { HeaderHomeButton } from './header-home-button/header-home-button'
+import { HeaderHomeButton } from './header-home-button/header-home-button';
+import { HeaderButton } from './header-button/header-button';
+
 export const Header: VFC<{}> = () => {
   return (
     <header className="header">
       <HeaderHomeButton />
+      <HeaderButton text="Create Test"/>
+      <HeaderButton text="Run Station"/>
+      <HeaderButton text="Run Test"/>
+      <HeaderButton text="Analyze Test"/>
     </header>
   );
 }
