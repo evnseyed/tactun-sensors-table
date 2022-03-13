1d3f11cd0c0eb344eb51fd67a77e8be6bae344b9
Evn Sey <evn.seyed@gmail.com>
2022-03-13 20:00:04 +0300

    feat(app): add sidebar to app

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import './app.scss';
+
+import './app.styles.scss';
 
 import { Header } from './header/header';
+import { Sidebar } from './sidebar/sidebar'
+import { Title } from './title/title'
+
+import { useTitle } from './hooks/use-title'
 
 export const App = () => {
+  const title = useTitle();
+
   return (
     <div className="app">
       <Header />
+
+      <div className="app__container">
+        <Sidebar />
+        <main className="app__main">
+          
+        </main>
+      </div>
+      {/* <main className="app__main">
+        
+        <Title text={title} className="app__title" />
+      </main> */}
     </div>
   );
 }
