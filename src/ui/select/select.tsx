81b2d26a5755bd977b88d6f781233b625f404127
Evn Sey <evn.seyed@gmail.com>
2022-03-21 13:41:33 +0300

    feat(ui): create select

---
+++ src/ui/select/select.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react';
+
+import { ItemRenderer, Select as BlueprintjsSelect } from '@blueprintjs/select'
+
+import './select.styles.scss';
+import { Button, ButtonProps } from '../button/button';
+import { ArrowHollowDownIcon } from '../../icons';
+
+interface SelectProps<T> {
+  items: T[],
+  itemRenderer: ItemRenderer<T>,
+  onItemSelect: (item: T) => void,
+  buttonProps: ButtonProps
+}
+
+export const Select = <T,>({ items, itemRenderer, onItemSelect, buttonProps }: SelectProps<T>): JSX.Element => {
+  const TypedSelect = useRef(BlueprintjsSelect.ofType<T>());
+
+  return (
+    <TypedSelect.current
+      items={items}
+      itemRenderer={itemRenderer}
+      onItemSelect={onItemSelect}
+      filterable={false}
+    >
+      <Button {...buttonProps} rightIcon={<ArrowHollowDownIcon/>} />
+    </TypedSelect.current>
+  );
+};
